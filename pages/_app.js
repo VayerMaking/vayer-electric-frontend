e7831c0601e7dd14411309be4de7d9468e54e150
VayerMaking <martin.vayer@gmail.com>
2023-08-08 18:59:02 +0300

    test safehydrate

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,21 @@ import '../styles/globals.css'
 import Layout from '../layouts/layout'
 import {fetchCategoriesNameArray} from '../utils/categoryProvider'
 
+function SafeHydrate({ children }) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === 'undefined' ? null : children}
+    </div>
+  )
+}
+
 function Ecommerce({ Component, pageProps, categories }) {
   return (
+    <SafeHydrate>
     <Layout categories={categories}>
       <Component {...pageProps} />
     </Layout>
+    </SafeHydrate>
   )
 }
 
